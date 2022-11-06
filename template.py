9cfe366c7edb035c274ceeecd8616839fe9cc7b8
BenSiv <bensiv92@gmail.com>
2022-11-06 18:07:08 +0200

    added template script

---
+++ template.py
@@ -0,0 +1,17 @@
+"""
+Broad summary for the script
+"""
+
+# import modules
+
+# functions
+
+# tests
+
+def main():
+    """
+    Runs the script through
+    """
+
+if __name__ == "__main__":
+    main()
