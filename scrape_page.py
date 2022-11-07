7baee3065f5fc9605776f50d7e3b3759ab2e8bbc
Omri Abo <omriabo.gmail.com>
2022-11-07 16:43:28 +0200

    fields added to scrape_page

--- scrape_page.py
+++ scrape_page.py
@@ -3,14 +3,19 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    url = "https://www.drugs.com/mtm/ampicillin.html"
+    url = "https://www.drugs.com/mtm/anastrozole.html"
     page = requests.get(url)
 
     soup = BeautifulSoup(page.content, "html.parser")
     content = soup.find(id="content")
-    title_head = content.find("div", class_="contentHead")
-    title = title_head.find("li", class_="ddc-breadcrumb-item active")
+    info = content.find("div", class_="contentBox")
+    title = info.find("div", class_="ddc-pronounce-title")
     print(title.text)
+    subtitle = info.find("p", class_="drug-subtitle")
+    print(subtitle.text)
+    related_treatments = info.find("ul", class_="more-resources-list more-resources-list-conditions")
+    print(related_treatments.text)
+
 
 
 if __name__ == "__main__":
