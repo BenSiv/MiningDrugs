3184c65291f78757c16ef81611cc6d980ad8a9fa
Omri Abo <omriabo.gmail.com>
2022-11-09 14:30:24 +0200

    scrape_page updated

--- scrape_page.py
+++ scrape_page.py
@@ -2,10 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main():
-    url = "https://www.drugs.com/mtm/anastrozole.html"
+def scrape_page(url):
     page = requests.get(url)
-
     soup = BeautifulSoup(page.content, "html.parser")
     content = soup.find(id="content")
     info = content.find("div", class_="contentBox")
@@ -15,7 +13,19 @@ def main():
     print(subtitle.text)
     related_treatments = info.find("ul", class_="more-resources-list more-resources-list-conditions")
     print(related_treatments.text)
+    related_drugs = info.find("div", class_="ddc-inset-text")
+    print(related_drugs.text)
+    """side_effects = info.find("div", class_="contentBox")
+    print(side_effects.text)
+    more_resources = info.find("div", class_="more-resources")
+    print(more_resources.text)"""
+    # the last two are in info.text but can't find them
+    return 'success'
 
+
+def main():
+    info_text = scrape_page("https://www.drugs.com/mtm/anastrozole.html")
+    print(info_text)
 
 
 if __name__ == "__main__":
